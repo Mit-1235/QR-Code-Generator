c8e6ead8a8591c52bb2edec756f35efd9a079014
Mit-1235 <mitkalathiya2104@gmail.com>
2024-05-24 17:48:34 +0530

    first upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+let imgBox = document.getElementById("imgbox");
+let qrtext = document.getElementById("textbox");
+let qrImage = document.getElementById("qrImage");
+
+
+function genarateQR(){
+
+    if(qrtext.value.length > 0 ){
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
+
+    imgBox.classList.add("show-img");
+    }
+    else{
+        qrtext.classList.add("error");
+        setTimeout(() => {
+            qrtext.classList.remove("error");
+        }, 1000);
+    }
+
+}
